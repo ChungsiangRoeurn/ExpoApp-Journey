2b839e32464c83a93bdad374523f936c4795c400
Chungsiang Roeurn <rn.chungsiang@gmail.com>
2025-12-28 17:23:39 +0700

    feat: Add dark mode toggle to settings and integrate Convex for backend functionality

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import useTheme from "@/hooks/useTheme";
+import React from "react";
+import { StyleSheet, Switch, Text, View } from "react-native";
 
 export default function Settings() {
+  const { isDarkMode, toggleDarkMode } = useTheme();
+
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={styles.container}>
+      {/* Section Title */}
+      <Text style={styles.sectionTitle}>Appearance</Text>
+
+      {/* Dark Mode Item */}
+      <View style={styles.settingItem}>
+        <Text style={styles.settingText}>Dark Mode</Text>
+
+        <Switch value={isDarkMode} onValueChange={toggleDarkMode} />
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 12,
+  },
+  settingItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 14,
+  },
+  settingText: {
+    fontSize: 16,
+  },
+});
